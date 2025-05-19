224d0529e8faf47ebe89f9f7c750e5cd0a52c2be
angel.delgado <angeldadaro@gmail.com>
2025-05-19 17:56:49 -0400

    fast fix

--- src/types/command.go
+++ src/types/command.go
@@ -16,10 +16,5 @@ type CommandFactory func() Command
 type CommandBus interface {
 	Dispatch(command Command) error
 	RegisterHandler(messageName string, handler CommandHandler, factory CommandFactory) error
-
-	// PreRegisterProducer pre-registra un productor para un tipo de comando específico
-	PreRegisterProducer(messageName string) error
-
-	// Close libera los recursos utilizados por el bus de comandos
 	Close() error
 }
